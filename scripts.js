656ab805d1ae2924a93e8cf8b312a7ee7801d805
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-02-08 14:55:25 +0600

    added keydown, keyup

--- scripts.js
+++ scripts.js
@@ -2,6 +2,27 @@ let button = document.querySelector('button');
 let fullName = document.querySelector('#full-name');
 
 button.addEventListener('click', function(event){
-  console.log(event);
-  event.preventDefault();
+     event.preventDefault();
+});
+fullName.addEventListener("keydown", function(event){ //при нажатии на клавищу
+    console.log(event.code);
+    console.log(event.key);
+   if (event.key == 'z') {
+       event.preventDefault();
+   }
+});
+fullName.addEventListener("keyup", function(event){//при изминения вводда
+    console.log("keyup");
+});
+fullName.addEventListener("change", function(event){ // при отпускании клавища
+    console.log("change");
+});
+fullName.addEventListener("input", function(event){// при изминения поля потря фокуса
+    console.log("input");
+});
+fullName.addEventListener("copy", function(event){//при копировании 
+    console.log("copy");
+});
+fullName.addEventListener("paste", function(event){//при вставке 
+    console.log("paste");
 });
