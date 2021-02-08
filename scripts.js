fbe562e2cc4eabb513c8fe4856436abcbf8062fc
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-02-08 15:06:42 +0600

    we output to the console

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ let button = document.querySelector('button');
 let fullName = document.querySelector('#full-name');
 
 button.addEventListener('click', function(event){
+    console.log(event);
      event.preventDefault();
 });
 fullName.addEventListener("keydown", function(event){ //при нажатии на клавищу
