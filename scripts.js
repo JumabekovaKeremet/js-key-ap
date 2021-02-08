c540de7140343a69dd16566ddf9f03aecc697ffd
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-02-08 15:42:17 +0600

    changet JavaScript

--- scripts.js
+++ scripts.js
@@ -1,29 +1,56 @@
 let button = document.querySelector('button');
 let fullName = document.querySelector('#full-name');
+let dob = document.querySelector('#dob');
+let city = document.querySelector('#city');
+let form = document.querySelector('form');
 
-button.addEventListener('click', function(event){
+city.value = "karakol";
+// city.selectedIndex = 1;
+// city.options[1].selected = true;
+
+button.addEventListener('click', function(event) {
+    let r = Math.random() * 255;
+    let b = Math.random() * 255;
+    let g = Math.random() * 255;
+
+    // let rgb = rgb($ r}, ${g.toFixed(0)}, ${b.toFixed(0)});
+    console.log(rgb);
+});
+
+city.addEventListener('keydown', function(event) { // при нажатии на клавищу
+    console.log('keydown');
+    // if (event.code.match(/Key/) && !event.key.match(/[а-яА-Я]/)) {
+    //     event.preventDefault();
+    // }
+    // if (fullName.value.length > 30) {
+    //     event.preventDefault();
+    // }
+});
+city.addEventListener('input', function(event) { // при измении поля ввода
+    console.log('input');
+});
+city.addEventListener('keyup', function(event) { // при отпускании клавищи
+    console.log('keyup');
+});
+city.addEventListener('change', function(event) { // при изменении поля (потеря фокуса)
+    console.log('change');
+});
+city.addEventListener('copy', function(event) { // при копировании
     console.log(event);
-     event.preventDefault();
 });
-fullName.addEventListener("keydown", function(event){ //при нажатии на клавищу
-    console.log(event.code);
-    console.log(event.key);
-   if (event.key == 'z') {
-       event.preventDefault();
-   }
+city.addEventListener('paste', function(event) { // при вставке
+    console.log('paste');
 });
-fullName.addEventListener("keyup", function(event){//при изминения вводда
-    console.log("keyup");
+city.addEventListener('cut', function(event) { // при вырезании
+    console.log('cut');
 });
-fullName.addEventListener("change", function(event){ // при отпускании клавища
-    console.log("change");
+fullName.addEventListener('focus', function(event) { // при фокусе
+    console.log('focus');
 });
-fullName.addEventListener("input", function(event){// при изминения поля потря фокуса
-    console.log("input");
+fullName.addEventListener('blur', function(event) { // при потере фокуса
+    console.log('blur');
 });
-fullName.addEventListener("copy", function(event){//при копировании 
-    console.log("copy");
-});
-fullName.addEventListener("paste", function(event){//при вставке 
-    console.log("paste");
+form.addEventListener('submit', function(event) { // при отправке формы
+    console.log('submit');
+    event.preventDefault();
 });
